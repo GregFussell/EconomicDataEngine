0a000e512fa2454a7eb884cff980dc148bc81d15
jmcacioppo <jmcacioppo@bellsouth.net>
2017-03-06 00:57:17 -0500

    can scrape one community at a time

--- scraper.py
+++ scraper.py
@@ -7,8 +7,17 @@ def makesoup(url):
 	soupdata = BeautifulSoup(page, "html.parser")
 	return soupdata
 
-soup = makesoup("http://www.usboundary.com/Areas/Public%20Use%20Microdata%20Area/Florida")
+# soup = makesoup("http://www.usboundary.com/Areas/Public%20Use%20Microdata%20Area/Florida")
 
-for list1 in soup.find('ul', {"class":"areas"}):
-	for item in soup.find_all('li'):
-		print(item.text);
+# for ulitem in soup.find_all('ul', {'class':'areas'}):
+# 	for liitem in ulitem.find_all('li'):
+# 		print(liitem.text)
+
+soup = makesoup("http://www.usboundary.com/Areas/Public%20Use%20Microdata%20Area/Florida/Alachua%20County%20%28Central%29--Gainesville%20City%20%28Central%29%20PUMA/192542")
+
+communityID = soup.find_all('td')[1]
+name = soup.find_all('td')[3]
+belongsTo = soup.find_all('td')[5]
+
+result = [communityID.text, name.text, belongsTo.text]
+print(result)
