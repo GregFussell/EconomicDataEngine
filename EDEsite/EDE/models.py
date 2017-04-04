18350861943f00e44b263554c086ddbd564f8924
unknown <rossello.ac@gmail.com>
2017-04-04 12:40:50 -0400

    added static

--- EDEsite/EDE/models.py
+++ EDEsite/EDE/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+#class Household(models.Model):
+#    serialNo = models.CharField(primary_key = True, max_length=64)
+#    year = models.IntegerField(primary_key = True)
+#    numberOfMembers = models.IntegerField(primary_key=False)
+#    propertyValue = models.IntegerField(primary_key=False)
+#    isWithin = models.IntegerField(primary_key=False)
+#    numberOfChildren = models.IntegerField(primary_key=False)
--- EDEsite/EDE/views.py
+++ EDEsite/EDE/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 # Create your views here.
 def index(request):
-    html = open('EDESite/index.html')
-    return html
+    template = loader.get_template('EDE/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
