cc2534dc0b27b9a4769951a38c61ff159524a2ec
unknown <rossello.ac@gmail.com>
2017-04-04 11:53:09 -0400

    added templates

--- EDEsite/EDE/views.py
+++ EDEsite/EDE/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    html = open('..\..\EconomicDataEngine\index.html')
-    return HttpResponse("html")
+    html = open('EDESite/index.html')
+    return html
